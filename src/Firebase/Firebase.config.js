d89996e9987f60a610272265831d87a990168a4f
humashaaa <afridakhan2121@gmail.com>
2024-04-17 07:50:58 +0600

    first commit

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCCvRllTSshDr5VO8YJRl4I7b7lHDqpst8",
+  authDomain: "assignment-9-e795a.firebaseapp.com",
+  projectId: "assignment-9-e795a",
+  storageBucket: "assignment-9-e795a.appspot.com",
+  messagingSenderId: "14227988156",
+  appId: "1:14227988156:web:7a2e04ec533bd7d4f67ab9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+export default auth
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import Nav from "../Components/Nav";
+
+const Home = () => {
+    return (
+        <div>
+            <Nav></Nav>
+            <h1 className="text-3xl">this is home</h1>
+        </div>
+    );
+};
+
+export default Home;
